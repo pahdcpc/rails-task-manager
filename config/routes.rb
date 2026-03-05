e10355ae45b1ce672466eeee20ec5fa21e3adeb5
Paulo Coelho <pahdcpc@gmail.com>
2026-03-05 19:37:28 +0000

    rails task manager 0

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # 1. Listar todas as tarefas
+  get "tasks", to: "tasks#index"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # 2. Criar uma tarefa (O 'new' deve vir antes do 'show')
+  get "tasks/new", to: "tasks#new", as: :new_task
+  post "tasks", to: "tasks#create"
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # 3. Ver uma tarefa específica
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  # 4. Editar uma tarefa
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+
+  # 5. Eliminar uma tarefa
+  delete "tasks/:id", to: "tasks#destroy"
 end
